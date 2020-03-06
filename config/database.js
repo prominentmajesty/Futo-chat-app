766f5a6b43b099a950c8d90ddd98264ef263e0b3
MAJESTY <ugochukwuaugustine7@gmail.com>
2020-03-06 01:40:23 -0800

    another commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/helenchatapp',
+    database: 'mongodb+srv://majesty:majesty@chart-app-2gh7e.mongodb.net/test?retryWrites=true&w=majority',
     // database: 'mongodb://barman:VICEcity47@ds123783.mlab.com:23783/helenchatapp',
     secret: 'barman secret'
 };
